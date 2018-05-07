832d0d64f6dae9b19a9d064865a392095ca430ea
oleg <ukhanyov.oleg@gmail.com>
2018-05-07 09:04:55 +0300

    Weather Data access object (dao)

---
+++ app/src/main/java/com/example/oleg/weatherapp_demo/data/WeatherDao.java
@@ -0,0 +1,22 @@
+package com.example.oleg.weatherapp_demo.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WeatherDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Weather weather);
+
+    @Query("DELETE FROM weather_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM weather_table ORDER BY date ASC")
+    LiveData<List<Weather>> getAllWeather();
+}
