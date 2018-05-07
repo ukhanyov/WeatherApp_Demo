5f29de2819ebce3bfc74e1f78ce89c5fa094a3aa
oleg <ukhanyov.oleg@gmail.com>
2018-05-07 09:32:31 +0300

    Weather view model

---
+++ app/src/main/java/com/example/oleg/weatherapp_demo/data/WeatherViewModel.java
@@ -0,0 +1,26 @@
+package com.example.oleg.weatherapp_demo.data;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+// Timeline 5
+
+public class WeatherViewModel extends AndroidViewModel {
+
+    private WeatherRepository mRepository;
+    private LiveData<List<Weather>> mAllWeather;
+
+    public WeatherViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new WeatherRepository(application);
+        mAllWeather = mRepository.getAllWeather();
+    }
+
+    public void insert(Weather weather) {mRepository.insert(weather);}
+
+    public void deleteAll() {mRepository.deleteAll();}
+}
