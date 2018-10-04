623294b1f5c79e616f8afaad44d4d38f08fa0b0b
ukhanyov <ukhanyov.oleg@gmail.com>
2018-10-04 13:59:19 +0300

    Made month look nice

--- app/src/main/java/com/example/oleg/weatherapp_demo/utils/NormalizeDate.java
+++ app/src/main/java/com/example/oleg/weatherapp_demo/utils/NormalizeDate.java
@@ -10,7 +10,7 @@ public class NormalizeDate {
         calendar.setTimeInMillis(dbDate * 1000L);
         return getFriendlyDayOfTheWeek(calendar.get(Calendar.DAY_OF_WEEK)) + "-" +
                 calendar.get(Calendar.DAY_OF_MONTH) + "-" +
-                calendar.get(Calendar.MONTH) + "-" +
+                getFriendlyMonth(calendar.get(Calendar.MONTH)) + "-" +
                 calendar.get(Calendar.YEAR);
     }
 
@@ -30,6 +30,37 @@ public class NormalizeDate {
                 return "Fri";
             case 7:
                 return "Sat";
+            default:
+                return "NaN";
+        }
+    }
+
+    private static String getFriendlyMonth(int key){
+        switch (key) {
+            case 0:
+                return "Jan";
+            case 1:
+                return "Feb";
+            case 2:
+                return "Mar";
+            case 3:
+                return "Apr";
+            case 4:
+                return "May";
+            case 5:
+                return "Jun";
+            case 6:
+                return "Jul";
+            case 7:
+                return "Aug";
+            case 8:
+                return "Sept";
+            case 9:
+                return "Oct";
+            case 10:
+                return "Nov";
+            case 11:
+                return "Dec";
             default:
                 return "NaN";
         }
